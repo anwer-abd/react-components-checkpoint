de1be4a535a352b278d268877ad972bb8ba7eb8e
anwer-abd <81585706+anwer-abd@users.noreply.github.com>
2021-05-17 11:03:16 +0200

    Add files via upload

---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,9 @@
+import React from "react";
+const FullnameComponent = () => {
+    return (
+        <>
+            <h2>Gaaloul Najmeddine</h2>
+        </>
+    );
+};
+export default FullnameComponent;
---
+++ src/Component/Profile/ProfilPhoto.js
@@ -0,0 +1,9 @@
+import React from "react";
+const MyPhotoComponent = () => {
+    return (
+        <>
+            <img src={'./react_logo.png'} alt='IMG' width="200px"/>
+        </>
+    );
+};
+export default MyPhotoComponent;
